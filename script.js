243efa09ecc6ac2aae11a702274182c0ac74233b
Shruti Kushwaha <shruti11kushwaha8@gmail.com>
2026-09-06 23:34:36 +0530

    goal added and footer

--- script.js
+++ script.js
@@ -11,3 +11,44 @@ document.getElementById("date").textContent = today.toLocaleDateString("en-US",{
 //profile
 let name = prompt("PLEASE ENTER YOUR FIRST NAME:");
 document.getElementById("profile").innerText= "HELLO, " + name.toUpperCase() + " !  WELCOME " + " TO TASK TRACKER !";
+
+//steps
+let steps = 0;
+let calories = 0;
+let water = 0;
+function addSteps(amount) {
+    steps+=amount;
+    if (steps>10000)
+    {
+        steps=10000;
+    }
+    document.getElementById("number").textContent=steps;
+    let percentage= (steps/10000)*100;
+    document.getElementById("stepBar").style.width = percentage+"%";
+}
+
+//waterintake
+function addWater(amount){
+    water+=amount;
+    if (water>2.5)
+    {
+        water=2.5;
+    }
+    document.getElementById("drink").textContent=water;
+    let percentage= (water/2.5)*100;
+    document.getElementById("waterBar").style.width= percentage+"%";
+}
+//calroies
+function addCalories(amount)
+{
+    calories+=amount;
+    if (calories>2400){
+        calories=2400;
+    }
+    document.getElementById("eat").textContent=calories;
+    let percentage= (calories/2400)*100;
+    document.getElementById("calorieBar").style.width=percentage + "%";
+}
+
+
+
